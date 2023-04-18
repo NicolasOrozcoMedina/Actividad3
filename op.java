51f77b387527a4dbea9738a56b6ddac541c80f5f
NicolasOrozcoMedina <125750000+NicolasOrozcoMedina@users.noreply.github.com>
2023-04-17 22:55:54 -0500

    Add files via upload

---
+++ op.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio40;
+
+/**
+ *
+ * @author Nicolás
+ */
+public class op {
+    public static double raiz(double a){
+        a=Math.sqrt(a);
+    return a;
+    }
+    public static double cuadrado(double a){
+        a=Math.pow(a, 2);
+    return a;
+    }
+    public static double cubo(double a){
+        a=Math.pow(a, 3);
+    return a;
+    }
+}

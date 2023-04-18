e1a5311bde6cf6e8b077100a085976908bbf2ed8
NicolasOrozcoMedina <125750000+NicolasOrozcoMedina@users.noreply.github.com>
2023-04-17 22:54:52 -0500

    Add files via upload

---
+++ comp.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio22;
+
+/**
+ *
+ * @author Nicolás
+ */
+public class comp {
+    public static boolean check(double a,double b){
+        boolean t;
+        t=false;
+        if (a*b>450000){
+            t=true;
+        }
+    return t;   
+    }
+    
+}

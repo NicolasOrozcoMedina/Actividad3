5085d745db9856463a096cfb6f348519445602ed
NicolasOrozcoMedina <125750000+NicolasOrozcoMedina@users.noreply.github.com>
2023-04-17 22:53:45 -0500

    Add files via upload

---
+++ op.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio19;
+
+/**
+ *
+ * @author Nicolás
+ */
+public class op {
+    public static double al(double a){
+        double b;
+        b=Math.sqrt(3);
+        a=(b*a)/2;
+    return a;
+    }
+    public static double ar(double a){
+        a=(Math.sqrt(3)/4)*Math.pow(a, 2);
+    return a;
+    }
+}

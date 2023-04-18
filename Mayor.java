b1bfa742f59cbcd1a50683249cab17e03f9c0eba
NicolasOrozcoMedina <125750000+NicolasOrozcoMedina@users.noreply.github.com>
2023-04-17 22:56:17 -0500

    Add files via upload

---
+++ Mayor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio41;
+
+/**
+ *
+ * @author Nicolás
+ */
+public class Mayor {
+    public static double comp(double a,double b,double c,double d){
+        double v1,v2,max;
+        v1=Math.max(a, b);
+        v2=Math.max(c, d);
+        max=Math.max(v1, v2);
+    return max;
+    }
+    
+}

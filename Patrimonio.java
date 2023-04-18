18ea07555aba951c8be0ccdb83a456e2ebab5516
NicolasOrozcoMedina <125750000+NicolasOrozcoMedina@users.noreply.github.com>
2023-04-17 22:51:39 -0500

    Add files via upload

---
+++ Patrimonio.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio10;
+
+/**
+ *
+ * @author Nicolás
+ */
+public class Patrimonio {
+    public static double precio(double a,double b){
+        double c,d;
+        c=50000;
+        if (a>2000000 && b>3){
+            d=(a*0.03);
+            c+=d; 
+        } 
+    return c;    
+    }
+    
+}

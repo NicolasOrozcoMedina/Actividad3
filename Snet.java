ce4561bcf87a6d9af6e40cba6a39812384d78728
NicolasOrozcoMedina <125750000+NicolasOrozcoMedina@users.noreply.github.com>
2023-04-17 22:52:29 -0500

    Add files via upload

---
+++ Snet.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio18;
+
+/**
+ *
+ * @author Nicolás
+ */
+public class Snet {
+    public static double salar(double a, double b, double c){
+        double d,e;
+        d=a*b;
+        e=d-(d*c);  
+    return e;
+    }
+    
+}
